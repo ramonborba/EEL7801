f1ad132946af4e0a51d0a68bd2b78f4e07de02db
Ramon Borba <ramonborba97@gmail.com>
2022-06-10 00:14:16 -0300

    Added remaining tasks to create_tasks function

--- SmartDucha/tasks/tasks_api/tasks_api.cpp
+++ SmartDucha/tasks/tasks_api/tasks_api.cpp
@@ -25,6 +25,18 @@
 
 void create_tasks()
 {
+#if defined(CONFIG_TASK_STARTUP_ENABLE) && (CONFIG_TASK_STARTUP_ENABLE == 1)
+    xTaskCreatePinnedToCore(vTaskStartup,
+                            TASK_STARTUP_NAME,        // Task name
+                            TASK_STARTUP_STACK_SIZE,  // Task stack size
+                            NULL,                       // Task optional parameters
+                            TASK_STARTUP_PRIORITY,    // Task priority
+                            &xTaskStartupHandle,      // Task handle poiter
+                            TASK_STARTUP_CORE);       // Task core affinity
+
+#endif  /* CONFIG_TASK_STARTUP_ENABLE */
+
+
 #if defined(CONFIG_TASK_HEARTBEAT_ENABLE) && (CONFIG_TASK_HEARTBEAT_ENABLE == 1)
     xTaskCreatePinnedToCore(vTaskHeartbeat,
                             TASK_HEARTBEAT_NAME,        // Task name
@@ -36,15 +48,52 @@ void create_tasks()
 
 #endif  /* CONFIG_TASK_HEARTBEAT_ENABLE */
 
-#if defined(CONFIG_TASK_STARTUP_ENABLE) && (CONFIG_TASK_STARTUP_ENABLE == 1)
-    xTaskCreatePinnedToCore(vTaskHeartbeat,
-                            TASK_HEARTBEAT_NAME,        // Task name
-                            TASK_HEARTBEAT_STACK_SIZE,  // Task stack size
-                            NULL,                       // Task optional parameters
-                            TASK_HEARTBEAT_PRIORITY,    // Task priority
-                            &xTaskHeartbeatHandle,      // Task handle poiter
-                            TASK_HEARTBEAT_CORE);       // Task core affinity
 
-#endif  /* CONFIG_TASK_STARTUP_ENABLE */
+#if defined(CONFIG_TASK_APP_COMM_ENABLE) && (CONFIG_TASK_APP_COMM_ENABLE == 1)
+    xTaskCreatePinnedToCore(vTaskAppComm,
+                            TASK_APP_COMM_NAME,        // Task name
+                            TASK_APP_COMM_STACK_SIZE,  // Task stack size
+                            NULL,                       // Task optional parameters
+                            TASK_APP_COMM_PRIORITY,    // Task priority
+                            &xTaskAppCommHandle,      // Task handle poiter
+                            TASK_APP_COMM_CORE);       // Task core affinity
+
+#endif  /* CONFIG_TASK_APP_COMM_ENABLE */
+
+
+#if defined(CONFIG_TASK_INTERFACE_ENABLE) && (CONFIG_TASK_INTERFACE_ENABLE == 1)
+    xTaskCreatePinnedToCore(vTaskInterface,
+                            TASK_INTERFACE_NAME,        // Task name
+                            TASK_INTERFACE_STACK_SIZE,  // Task stack size
+                            NULL,                       // Task optional parameters
+                            TASK_INTERFACE_PRIORITY,    // Task priority
+                            &xTaskInterfaceHandle,    // Task handle poiter
+                            TASK_INTERFACE_CORE);       // Task core affinity
+
+#endif  /* CONFIG_TASK_INTERFACE_ENABLE */
+
+
+#if defined(CONFIG_TASK_POWER_CONTROL_ENABLE) && (CONFIG_TASK_POWER_CONTROL_ENABLE == 1)
+    xTaskCreatePinnedToCore(vTaskPowerControl,
+                            TASK_POWER_CONTROL_NAME,        // Task name
+                            TASK_POWER_CONTROL_STACK_SIZE,  // Task stack size
+                            NULL,                       // Task optional parameters
+                            TASK_POWER_CONTROL_PRIORITY,    // Task priority
+                            &xTaskPowerControlHandle,    // Task handle poiter
+                            TASK_POWER_CONTROL_CORE);       // Task core affinity
+
+#endif  /* CONFIG_TASK_POWER_CONTROL_ENABLE */
+
+
+#if defined(CONFIG_TASK_READ_SENSORS_ENABLE) && (CONFIG_TASK_READ_SENSORS_ENABLE == 1)
+    xTaskCreatePinnedToCore(vTaskReadSensors,
+                            TASK_READ_SENSORS_NAME,        // Task name
+                            TASK_READ_SENSORS_STACK_SIZE,  // Task stack size
+                            NULL,                       // Task optional parameters
+                            TASK_READ_SENSORS_PRIORITY,    // Task priority
+                            &xTaskReadSensorsHandle,    // Task handle poiter
+                            TASK_READ_SENSORS_CORE);       // Task core affinity
+
+#endif  /* CONFIG_TASK_INTERFACE_ENABLE */
 
 }
