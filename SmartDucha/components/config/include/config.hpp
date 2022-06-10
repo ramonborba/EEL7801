f1fee4572a721617f41569801af96555dbb9ab9b
Ramon Borba <ramonborba97@gmail.com>
2022-06-10 00:00:18 -0300

    Created config component

---
+++ SmartDucha/components/config/include/config.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file config.hpp
+ * 
+ * @brief Application configuration file
+ * 
+ * @author Ramon de Araujo Borba <ramonborba97@gmail.com>
+ * 
+ * @version 0.1
+ * 
+ * @date 09-06-2022
+ */
+#ifndef CONFIG_HPP_
+#define CONFIG_HPP_
+
+/* Task Enables */
+#define CONFIG_TASK_STARTUP_ENABLE          0
+#define CONFIG_TASK_HEARTBEAT_ENABLE        1
+#define CONFIG_TASK_APP_COMM_ENABLE         0
+#define CONFIG_TASK_INTERFACE_ENABLE        0
+#define CONFIG_TASK_POWER_CONTROL_ENABLE    0
+#define CONFIG_TASK_READ_SENSORS_ENABLE     0
+
+#endif /* CONFIG_HPP_ */
