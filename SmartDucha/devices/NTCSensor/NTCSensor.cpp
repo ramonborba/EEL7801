6ba9963bd2e77bdd435495e0844f0105f4d81142
Ramon Borba <ramonborba97@gmail.com>
2022-06-14 20:17:30 -0300

    Added 'devices' component folder to project structure, created NTCSensor device

---
+++ SmartDucha/devices/NTCSensor/NTCSensor.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file NTCSensor.cpp
+ * 
+ * @brief NTCSensor class
+ * 
+ * @author Ramon de Araujo Borba <ramonborba97@gmail.com>
+ * 
+ * @version 0.1
+ * 
+ * @date 14-06-2022
+ */
+
+#include <stdio.h>
+#include "NTCSensor.hpp"
+
+void func(void)
+{
+
+}
---
+++ SmartDucha/devices/NTCSensor/include/NTCSensor.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file NTCSensor.hpp
+ * 
+ * @brief NTCSensor class header file
+ * 
+ * @author Ramon de Araujo Borba <ramonborba97@gmail.com>
+ * 
+ * @version 0.1
+ * 
+ * @date 14-06-2022
+ */
+#ifndef NTCSENSOR_HPP_
+#define NTCSENSOR_HPP_
+
+#include <cstdint>
+
+
+
+#endif /* NTCSENSOR_HPP_ */
