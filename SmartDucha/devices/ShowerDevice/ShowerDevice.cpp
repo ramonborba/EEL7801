bed7e7684536f2ec9f78c7eaab62cb9c950ea4a4
Ramon Borba <ramonborba97@gmail.com>
2022-07-26 00:53:15 -0300

    Updated ShowerDevice

--- SmartDucha/devices/ShowerDevice/ShowerDevice.cpp
+++ SmartDucha/devices/ShowerDevice/ShowerDevice.cpp
@@ -12,13 +12,17 @@
 
 #include "freertos/FreeRTOS.h"
 #include "ShowerDevice.hpp"
+#include "config.hpp"
 #include "power_control.hpp"
+#include "read_sensors.hpp"
 #include "esp_rom_sys.h"
+#include "Triac.hpp"
+#include "esp_log.h"
 
+static const char* TAG = "ShowerDevice";
 
-ShowerDevice::ShowerDevice() : Potencia(), _config { }
+ShowerDevice::ShowerDevice() : Potencia(), _Toff { setInitialPower(30) }, _config { }
 {
-
 }
 
 ShowerDevice& ShowerDevice::getInstance()
@@ -39,23 +43,66 @@ ShowerConfig ShowerDevice::getConfig()
 
 void ShowerDevice::start()
 {
+    _Toff = setInitialPower(30);
+
+    // Create read sensors task
+#if defined(CONFIG_TASK_READ_SENSORS_ENABLE) && (CONFIG_TASK_READ_SENSORS_ENABLE == 1)
+    if( !xTaskCreatePinnedToCore(vTaskReadSensors,
+                            TASK_READ_SENSORS_NAME,        // Task name
+                            TASK_READ_SENSORS_STACK_SIZE,  // Task stack size
+                            NULL,                       // Task optional parameters
+                            TASK_READ_SENSORS_PRIORITY,    // Task priority
+                            &xTaskReadSensorsHandle,    // Task handle poiter
+                            TASK_READ_SENSORS_CORE) )       // Task core affinity
+    {
+        ESP_LOGE(TAG, "Error creating read sensors task!");
+    }
+#endif /* CONFIG_TASK_READ_SENSORS_ENABLE */
+
     // open valve
 
     // Create power control task
-    xTaskCreatePinnedToCore(vTaskPowerControl,
+#if defined(CONFIG_TASK_POWER_CONTROL_ENABLE) && (CONFIG_TASK_POWER_CONTROL_ENABLE == 1)
+    if ( !xTaskCreatePinnedToCore(vTaskPowerControl,
                             TASK_POWER_CONTROL_NAME,        // Task name
                             TASK_POWER_CONTROL_STACK_SIZE,  // Task stack size
                             NULL,                       // Task optional parameters
                             TASK_POWER_CONTROL_PRIORITY,    // Task priority
                             &xTaskPowerControlHandle,    // Task handle poiter
-                            TASK_POWER_CONTROL_CORE);       // Task core affinity
+                            TASK_POWER_CONTROL_CORE) )       // Task core affinity
+    {
+        ESP_LOGE(TAG, "Error creating power control task!");
+    }
+#endif /* CONFIG_TASK_POWER_CONTROL_ENABLE */
 }
 
 void ShowerDevice::stop()
 {
     // suspend power control task
     vTaskDelete(xTaskPowerControlHandle);
+    if ( xTaskPowerControlHandle )
+    {
+        ESP_LOGI(TAG, "Deleted power control task.");
+    }
+    else
+    {
+        ESP_LOGE(TAG, "Error deleting power control task!");
+    }
+
+    // delete read sensors task
+    vTaskDelete(xTaskReadSensorsHandle);
+    if ( xTaskReadSensorsHandle )
+    {
+        ESP_LOGI(TAG, "Deleted read sensors task.");
+    }
+    else
+    {
+        ESP_LOGE(TAG, "Error deleting read sensors task!");
+    }
     
+    // set triac to low
+    Triac& triac = Triac::getInstance();
+    triac.set_low();
     
     // close valve
 }
